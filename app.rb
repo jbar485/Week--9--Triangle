2fd9df7b10ca0d8b5c7182fbd35cba858a359db0
Student <example@example.com>
2019-12-16 14:30:06 -0800

    add working front end logic

--- app.rb
+++ app.rb
@@ -9,10 +9,10 @@ get('/') do
 end
 
 post("/answer") do
-  side1 = params(:side1)
-  side2 = params(:side2)
-  side3 = params(:side3)
+  side1 = params[:side1].to_i
+  side2 = params[:side2].to_i
+  side3 = params[:side3].to_i
   @triangle = Triangle.new(side1, side2, side3)
-  @triangle_final = triangle.triangle_type
+  @triangle_final = @triangle.triangle_type
   erb(:answerpage)
 end
